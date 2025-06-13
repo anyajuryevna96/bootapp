5fbe378eec2c25d9ddd63e8789f39d2e7b22a10e
AnnaSki <annajuryevna96@gmail.com>
2025-06-13 18:19:55 +0300

    list all sporty activities

--- app/controllers/sporty_types_controller.rb
+++ app/controllers/sporty_types_controller.rb
@@ -1,16 +1,32 @@
 class SportyTypesController < ApplicationController
+  before_action :set_sporty_type, only: %i[ show ]
+
   def index
     @sporty_types = SportyType.all
   end
-  def new
-    @sporty_types = SportActivity.new
+
+  def show
   end
+
+  def new
+    @sporty_type = SportyType.new
+  end
+
   def create
-    @sport_activit = SportActivity.new(sport_activity_params)
-    if @daily_activity.save
-      redirect_to @sport_activity
+    @sporty_type = SportyType.new(sporty_type_params)
+    if @sporty_type.save
+      redirect_to @sporty_type
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+  def sporty_type_params
+    params.expect(sporty_type: [ :name ])
+  end
+
+  def set_sporty_type
+    @sporty_type = SportyType.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "daily_activities#index"
   resources :daily_activities
+  resources :sporty_types
 end
