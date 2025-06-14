a587b9e35a121e6eba3046c8eb2c55082cd3ded1
AnnaSki <annajuryevna96@gmail.com>
2025-06-14 12:09:19 +0300

    Fixtures names improvement

--- test/controllers/daily_activities_controller_test.rb
+++ test/controllers/daily_activities_controller_test.rb
@@ -6,15 +6,25 @@ class DailyActivitiesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should create activity" do
+    st = sport_types(:walking)
+    assert_difference "DailyActivity.count", 1 do
+      post daily_activities_path, params: { daily_activity: { duration: 60, sport_type_id: st.id } }
+      assert_redirected_to daily_activity_path(DailyActivity.last)
+    end
+  end
+
   test "should get show" do
-    d = fixture(:daily_activities, :one)
+    d = daily_activities(:sigulda)
     get daily_activity_path(d)
     assert_response :success
   end
 
   test "should get destroy" do
-    d = fixture(:daily_activities, :one)
-    delete daily_activity_path(d)
-    assert_redirected_to daily_activities_path
+     d = daily_activities(:sigulda)
+     assert_difference "DailyActivity.count", -1 do
+     delete daily_activity_path(d)
+     assert_redirected_to daily_activities_path
+    end
   end
 end
